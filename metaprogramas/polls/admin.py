1b3cba3af0c1d00c69baaac3cf823658a8aa243a
Diego <dalvarez.cursos@gmail.com>
2020-11-26 12:02:04 -0400

    Limitación por usuario

--- metaprogramas/polls/admin.py
+++ metaprogramas/polls/admin.py
@@ -6,8 +6,19 @@ from .models import *
 
 
 class PollAdmin(admin.ModelAdmin):
-    pass
+
+    """def queryset(self, request):
+        qs = super(PollAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(owner=request.user)"""
+
+    def get_queryset(self, request):
+        qs = super(PollAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(author=request.user)
 
 
-admin.site.register(Polls)
+admin.site.register(Polls, PollAdmin)
 admin.site.register(Candidate)
